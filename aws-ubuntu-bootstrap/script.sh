3059478bc967f1215e6d1c8af14390b3f567c366
Nikita Baranov <nikita.baranov.devops@gmail.com>
2021-11-09 15:13:56 +0300

    Jenkinsfile and aws ubuntu boostrap bash script added

---
+++ aws-ubuntu-bootstrap/script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sh -c 'echo deb https://pkg.jenkins.io/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-get update
+apt install openjdk-11-jdk -y
+apt-get install jenkins -y
+apt install openssh-server -y
+ufw allow 8080
+ufw allow OpenSSH
+ufw enable
+
+apt-get install awscli -yaws
+
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update -y
+apt-get install docker-ce docker-ce-cli containerd.io -y
+usermod -aG docker jenkins
+
+apt-get install unzip
+wget https://releases.hashicorp.com/terraform/1.0.9/terraform_1.0.9_linux_amd64.zip
+unzip terraform_1.0.9_linux_amd64.zip
+rm terraform_1.0.9_linux_amd64.zip
+mv terraform /bin/
+
+echo 'jenkins    ALL=(ALL:ALL) ALL' >> /etc/sudoers
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
