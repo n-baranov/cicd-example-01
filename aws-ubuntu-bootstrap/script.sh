25465622428f889d264281b3411b97e920223cd4
Nikita Baranov <nikita.baranov.devops@gmail.com>
2021-11-10 11:28:22 +0300

    Healthcheck build #68

--- aws-ubuntu-bootstrap/script.sh
+++ aws-ubuntu-bootstrap/script.sh
@@ -38,3 +38,5 @@ echo 'jenkins    ALL=(ALL:ALL) ALL' >> /etc/sudoers
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+sudo usermod -aG docker jenkins
