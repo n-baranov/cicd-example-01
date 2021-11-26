50a2c13f60a7d93cb6003b70a284495c48e0e15f
Nikita Baranov <nikita.baranov.devops@gmail.com>
2021-11-26 21:43:09 +0300

    jenkinsfile refactoring...

--- healthcheck/healthcheck.sh
+++ healthcheck/healthcheck.sh
@@ -7,7 +7,7 @@ set -e
 LARAVEL_ENDPOINT=$1
 
 Counter=0
-until [ $(curl -LI $LARAVEL_ENDPOINT -o /dev/null -w '%{http_code}\n' -s) == "200" || $Counter -gt 60]; do sleep 10 && ((Counter++)); done
+until [ $(curl -LI $LARAVEL_ENDPOINT -o /dev/null -w '%{http_code}\n' -s) == "200" ] || [ $Counter -gt 60 ]; do sleep 10 && ((Counter++)); done
 
 if curl $LARAVEL_ENDPOINT 2>&1 | grep -q 'Laravel'
 then
