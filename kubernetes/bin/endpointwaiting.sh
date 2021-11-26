3b5890ef14bebba225c89ec0ef819107c441c6e9
Nikita Baranov <nikita.baranov.devops@gmail.com>
2021-11-26 17:50:55 +0300

    jenkinsfile refactoring...

--- kubernetes/bin/endpointwaiting.sh
+++ kubernetes/bin/endpointwaiting.sh
@@ -4,7 +4,7 @@ external_ip=""
 while [ -z $external_ip ]; do
   echo "Waiting for the LB to up..."
   external_ip=`kubectl get services laravel-lb --output jsonpath='{.status.loadBalancer.ingress[0].hostname}'`
-  [ -z "$external_ip" ] && sleep 10
+  [ -z $external_ip ] && sleep 10
 done
 echo 'End point ready:'
 echo $external_ip
