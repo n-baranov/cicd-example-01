e57e2c9bc63cb561837fb967301147c2acb7e30e
Nikita Baranov <nikita.baranov.devops@gmail.com>
2021-11-26 15:31:52 +0300

    jenkinsfile refactoring...

--- docker/bin/build.sh
+++ docker/bin/build.sh
@@ -8,10 +8,13 @@ build_folder=$1
 aws_ecr_repository_url_with_tag=$2
 dockerfile_name=$3
 
-#aws ecr create-repository \
-#--repository-name php-laravel \
-#--image-scanning-configuration scanOnPush=true \
-#--region ${region}
+if ( ! aws ecr describe-repositories | grep php-laravel )
+then
+   aws ecr create-repository \
+   --repository-name php-laravel \
+   --image-scanning-configuration scanOnPush=true \
+   --region ${region}
+fi
 
 # Check if aws is installed
 which aws > /dev/null || { echo 'ERROR: aws-cli is not installed' ; exit 1; }
@@ -23,7 +26,7 @@ $(aws ecr get-login --no-include-email) || { echo 'ERROR: aws ecr login failed' 
 which docker > /dev/null && docker ps > /dev/null || { echo 'ERROR: docker is not running' ; exit 1; }
 
 # Build image
-docker build -t $aws_ecr_repository_url_with_tag -f $dockerfile_name $build_folder
+docker build -t $aws_ecr_repository_url_with_tag -f $dockerfile_name $build_folder .
 
 # Push image
 docker push $aws_ecr_repository_url_with_tag
