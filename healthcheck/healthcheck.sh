0581dec7a886004ed5b4b284404b109dcb62cb00
Nikita Baranov <nikita.baranov.devops@gmail.com>
2021-11-29 15:36:33 +0300

    Refactoring...

--- healthcheck/healthcheck.sh
+++ healthcheck/healthcheck.sh
@@ -5,16 +5,16 @@ LARAVEL_ENDPOINT=$1
 
 Counter=1
 echo "Waiting for the \"200\" from Laravel. Attempt #$Counter of 60"
-until [ $(curl -LI $LARAVEL_ENDPOINT -o /dev/null -w '%{http_code}\n' -s) == "_200" ] || [ $Counter == 10 ]
+until [ $(curl -LI $LARAVEL_ENDPOINT -o /dev/null -w '%{http_code}\n' -s) == "200" ] || [ $Counter == 60 ]
 do
   ((Counter++))
-  echo "Waiting for the \"200\" response from Laravel. Attempt #$Counter of 10"
+  echo "Waiting for the \"200\" response from Laravel. Attempt #$Counter of 60"
 done
 
-if [ $Counter == 10 ]
+if [ $Counter == 60 ]
 then
-  echo "-------------------- Something's wrong. This deployment is not ok --------------------"
+  echo "-------------------- Something's wrong! This deployment is gonna be destroyed --------------------"
   exit 1
 else
-  echo "-------------------- This deployment is ok --------------------"
+  echo "-------------------- This deployment is ok and it's gonna be destroyed --------------------"
 fi
